429a6ff1bb7d83c7765c7f5a0b31e3551232bb6a
rikki cattermole <alphaglosined@gmail.com>
2017-01-23 22:55:59 +1300

    An example file

---
+++ source/app.d
@@ -0,0 +1,19 @@
+﻿module app;
+
+void main() {
+	import std.stdio : writeln;
+	import cubf.writer;
+	import cubf.reader;
+
+	CUBFWriter writer = new CUBFWriter;
+	writer.appendChunk("abcd", cast(ubyte[])"some chunk goes here");
+	writer.appendHeaderChunk("ihdr", cast(ubyte[])"this is my header");
+	writer.appendChunk("abcd", cast(ubyte[])"Hi there! you're cool right?");
+
+	import std.file : write, read;
+	write("test.cubf", writer.finalize());
+
+	foreach(v; CUBFReader(cast(ubyte[])read("test.cubf"))) {
+		writeln(v.name, "\t", cast(string)v.data, "\t", v.isHeader);
+	}
+}
